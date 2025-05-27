8d7930d734c73162e041d8a5c9c33eee16b511ea
Jerold Gomez <106787297+JeroldGomez@users.noreply.github.com>
2025-05-27 23:33:16 +0800

    Update Github Actions

--- app/components/ProjectCard.tsx
+++ app/components/ProjectCard.tsx
@@ -3,7 +3,6 @@
 import Image from "next/image";
 import { ArrowUpRightIcon, CodeBracketIcon } from "@heroicons/react/20/solid"; 
 
-
 interface ProjectCardProps {
     title: string;
     imageUrl: string;
