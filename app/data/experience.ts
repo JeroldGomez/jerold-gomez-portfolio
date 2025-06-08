225ae8d777618575e396c325b534835d29045912
Jerold Gomez <106787297+JeroldGomez@users.noreply.github.com>
2025-06-08 13:47:18 +0800

    Update experience.ts

--- app/data/experience.ts
+++ app/data/experience.ts
@@ -15,7 +15,7 @@ export const workExperience: Experience[] = [
         logo: "/cct_logo_t_main.svg",
         alt: "Company A Logo",
         title: "Data Analyst Intern · CCT",
-        companyLink: "https://example.com/companya",
+        companyLink: "",
         description: `Enhanced data integrity and operational efficiency by cleaning and processing large enrollment/academic datasets using Google Sheets. Developed interactive Power BI dashboards, transforming complex data into actionable insights that supported institutional decision-making.
 
         Contributed to a smoother onboarding experience by improving data capture processes for incoming freshmen and provided crucial systems support to maintain office productivity.`,
@@ -27,7 +27,7 @@ export const workExperience: Experience[] = [
         logo: "/cct_logo_t_main.svg",
         alt: "University Logo",
         title: "Junior IT Analyst · CCT",
-        companyLink: "https://example.edu/ai-lab",
+        companyLink: "",
         description: `Contributed to seamless IT infrastructure operations and strategic reporting. Spearheaded the creation of comprehensive reports on initiatives, projects, and achievements, delivering actionable insights that fostered informed decision-making by leadership.`,
         location: "Tagaytay City, Cavite",
         locationType: "office"
